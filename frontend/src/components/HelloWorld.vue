a6dbfba993c9d1e18bb60d2f84bc3c132d40fa43
sgash708 <sugash7081023@gmail.com>
2021-07-23 20:35:07 +0900

    基本構文の記述方法確認

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <p class="hello">{{ message }}</p>
+    <p>{{ number }}</p>
+    <!-- 下記のように判定文も記述可能 -->
+    <p>{{ ok ? 'YES' : 'NO' }}</p>
     <button v-on:click="reverseMess">メッセージ反転</button>
   </div>
 </template>
@@ -10,7 +13,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      message: 'Hello World!!'
+      message: 'Hello World!!',
+      number: 1,
+      ok: true
     }
   },
   methods: {
