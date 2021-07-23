cebba100f0f6ed3346f11de1ce8f1151abfef7f1
sgash708 <sugash7081023@gmail.com>
2021-07-23 18:41:47 +0900

    vue整備

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue.jsの再学習
+
+# REF
+
+https://zenn.dev/tet0h/articles/docker-vuejs
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="hello">{{ message }}</p>
+    <button v-on:click="reverseMess">メッセージ反転</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      message: 'Hello World!!'
+    }
+  },
+  methods: {
+    reverseMess: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
