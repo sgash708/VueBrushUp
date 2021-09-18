1c2db1235eef31932aedde6e3a956f5db1b9cb0b
sgash708 <sugash7081023@gmail.com>
2021-09-18 12:32:09 +0900

    命名規則変更

--- frontend/src/components/Section2.vue
+++ frontend/src/components/Section2.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p>hoge</p>
   </div>
 </template>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import BaseTech from '@/components/BaseTech'
-import Redndering from '@/components/Redndering'
+import Section1 from '@/components/Section1'
+import Section2 from '@/components/Section2'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'BaseTech',
-      component: BaseTech
+  // https://qiita.com/hshota28/items/765cf903f055754f7557/#導入
+  mode: 'history',
+  routes: [{
+      path: '/section',
+      name: 'Section1',
+      component: Section1
     },
     {
-      path: '/render',
-      name: 'Redndering',
-      component: Redndering
+      path: '/section2',
+      name: 'Section2',
+      component: Section2
     },
   ]
 })
