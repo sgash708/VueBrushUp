8efdb853a4ad6b7e42273a06297a110f934e37bc
sgash708 <sugash7081023@gmail.com>
2021-09-18 13:00:11 +0900

    l38 v-else-if

--- frontend/src/components/Section2.vue
+++ frontend/src/components/Section2.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <!-- v-elseはv-ifのすぐ下に来ないといけない -->
-    <p v-if="ok">OK</p>
+    <!-- v-else-ifでelseの間に挟むこと -->
+    <p v-if="ok">OK!!</p>
+    <p v-else-if="mayBeOk">MayBe OK!!</p>
     <p v-else>Not OK...</p>
   </div>
 </template>
@@ -11,7 +13,8 @@
     // REF: https://jp.vuejs.org/v2/guide/components.html#data-%E3%81%AF%E9%96%A2%E6%95%B0%E3%81%A7%E3%81%AA%E3%81%91%E3%82%8C%E3%81%B0%E3%81%AA%E3%82%8A%E3%81%BE%E3%81%9B%E3%82%93
     data() {
       return {
-        ok: false
+        ok: false,
+        mayBeOk: true,
       }
     }
   }
