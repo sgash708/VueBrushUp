082c2966bfaec79ae24249cafe9036a4be7a8986
sgash708 <sugash7081023@gmail.com>
2021-09-18 11:51:21 +0900

    セクション3用ファイル作成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import BaseTech from '@/components/BaseTech'
+import Redndering from '@/components/Redndering'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'BaseTech',
-    component: BaseTech
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'BaseTech',
+      component: BaseTech
+    },
+    {
+      path: '/render',
+      name: 'Redndering',
+      component: Redndering
+    },
+  ]
 })
