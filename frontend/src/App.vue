abb275c315ea9277cc0c4f0a53ec8be3e0008692
sgash708 <sugash7081023@gmail.com>
2021-10-16 11:52:17 +0900

    L97. propsの注意点

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,11 +7,11 @@
     <!-- L84. パスカルケース(RouterView)コンポーネントとした場合、ケバブケース(router-view)やキャメルケース(routerView)でも記述できる -->
     <!-- パスカルケース統一がおすすめ → HTMLは大文字小文字を認識できなく要素はケバブケースが多いので、一眼でコンポーネントだと理解できるため -->
     <!-- "しかし" JSfiddleなどでは、DOMテンプレートを使う際にはHTMLが先行して読み込まれるので"ケバブケース"にすること！ -->
-    <!-- <RouterView /> -->
+    <RouterView />
     <!-- ローカル登録は、App.vueに書く -->
     <!-- <Section6></Section6> -->
 
-    <Section7 :total-number="number" @my-click="incrementNumber"></Section7>
+    <!-- <Section7 :total-number="number" @my-click="incrementNumber"></Section7> -->
     <h1>トータルのいいね数</h1>
     <h2>{{number}}</h2>
 
@@ -19,52 +19,55 @@
 </template>
 
 <script>
-// import Section6 from '@/components/Section6.vue'
-import Section7 from '@/components/Section7.vue'
+  // import Section6 from '@/components/Section6.vue'
+  // import Section7 from '@/components/Section7.vue'
 
-export default {
-  name: 'App', 
-  /*
-    L81. ES6では同じ名前を定義することでコロンで記述しなくて良い
-    components: {
-      Section6: Section6
-    }
-  */
+  export default {
+    name: 'App',
+    /*
+      L81. ES6では同じ名前を定義することでコロンで記述しなくて良い
+      components: {
+        Section6: Section6
+      }
+    */
 
-  // L88. 親子間のコンポーネントでデータを受け渡す
-  // L90. propsはケバブケースで渡すのがオススメ
-  data() {
-    return {
-      number: 10
-    }
-  },
-  components: {
-    Section7
-  },
-  methods: {
-    incrementNumber(value) {
-      this.number = value
-    }
+    // L88. 親子間のコンポーネントでデータを受け渡す
+    // L90. propsはケバブケースで渡すのがオススメ
+    data() {
+      return {
+        number: 10
+      }
+    },
+    // components: {
+    //   Section7
+    // },
+    // methods: {
+    //   incrementNumber(value) {
+    //     this.number = value
+    //   }
+    // }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.red {
-  color: red;
-}
-.bg-blue {
-  background-color: blue;
-}
-/*
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .red {
+    color: red;
+  }
+
+  .bg-blue {
+    background-color: blue;
+  }
+
+  /*
 div {
   border: 1px solid blue;
 }
