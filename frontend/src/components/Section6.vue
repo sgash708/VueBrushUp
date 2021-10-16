f4401ada541caba7835e4ea56a8e16acbb293641
sgash708 <sugash7081023@gmail.com>
2021-10-16 09:16:23 +0900

    L75 コンポーネントの再使用

---
+++ frontend/src/components/Section6.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{number}}</p>
+</template>
+
+<script>
+  export default {
+    // L75. componentでは、"data"は関数として使用する
+    data() {
+      return {
+        number: 12
+      }
+    }
+  }
+</script>
