5f73b16a50487bcf2a0551162017ac9ced3f5ae7
sgash708 <sugash7081023@gmail.com>
2021-10-16 09:49:08 +0900

    L79, 単一コンポーネントの使用

--- frontend/src/components/Section6.vue
+++ frontend/src/components/Section6.vue
@@ -2,16 +2,19 @@
   <div>
     <p>{{number}}<button @click='increment()'>+1</button></p>
     <p>{{number}}<button @click='increment()'>+1</button></p>
-    <p>{{number}}<button @click='increment()'>+1</button></p>
   </div>
 </template>
 
 <script>
   // L77. グローバル登録
   export default {
-    // L75. componentでは、"data"は関数として使用する
     /*
+     * L75. componentでは、"data"は関数として使用する
      * L76. dataは初期値を入れる場所
+     * L79. Babelを使用しているので、ES6の書き方もできる。
+     * e.g.) <ES6> data()
+     * e.g.) <ES5> data: function () {}
+     * どちらでも同じ！
      */
     data() {
       return {
