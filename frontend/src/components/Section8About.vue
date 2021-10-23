a4c90f44774d648c05bd337065cd21b5435b4432
sgash708 <sugash7081023@gmail.com>
2021-10-23 11:29:59 +0900

    L116. ライフサイクルフック(activated/deactivated)

--- frontend/src/components/Section8About.vue
+++ frontend/src/components/Section8About.vue
@@ -7,12 +7,22 @@
 
 <script>
   export default {
+    // L115. keep-alivenによってdestroyedは実行されなくなる
     destroyed() {
       // L114. destoryによってコンポーネントを削除して再作成しているのでコストがかかる
       console.log("destroyed")
     },
     created() {
+      // L116. createdは一回は実行されるので注意すること。s
       console.log("created")
+    },
+    // L116. keep-aliveで状態保持した場合には、別のライフサイクルが適用される
+    // なので、"deactivated"と"activated"を使うようにする。
+    deactivated() {
+      console.log("deactivated!!")
+    },
+    activated() {
+      console.log("activated!!")
     }
   }
 </script>
