a92d9ceadec78f352069e986d4e6dc00767e2884
sgash708 <sugash7081023@gmail.com>
2021-11-14 13:01:27 +0900

    L120. .lazyイベントについて

--- frontend/src/components/Section9.vue
+++ frontend/src/components/Section9.vue
@@ -4,7 +4,9 @@
     <label for="title">タイトル</label>
 
     <!-- L119. 動的に値を書き換えることができる -->
-    <input id="title" type="text" v-model="eventData.title">
+    <!-- L120. ".lazy": focusが外れた時(changeイベント)で発火する -->
+    <!-- → メールアドレスの入力バリデーションなどはUXのために使うことができる -->
+    <input id="title" type="text" v-model.lazy="eventData.title">
     <p>{{eventData.title}}</p>
   </div>
 </template>
