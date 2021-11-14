0dc14466652c9b9710a4cb8fbbcb5a29609acbe8
sgash708 <sugash7081023@gmail.com>
2021-11-14 13:07:34 +0900

    L121. number修飾子

--- frontend/src/components/Section9.vue
+++ frontend/src/components/Section9.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h2>イベントのフォーム</h2>
-    <label for="title">タイトル</label>
-
     <!-- L119. 動的に値を書き換えることができる -->
     <!-- L120. ".lazy": focusが外れた時(changeイベント)で発火する -->
     <!-- → メールアドレスの入力バリデーションなどはUXのために使うことができる -->
+    <label for="title">タイトル</label>
     <input id="title" type="text" v-model.lazy="eventData.title">
     <p>{{eventData.title}}</p>
+
+    <!-- L121. ".number": カウントアップした時にstring型になってしまうのを防ぐ -->
+    <label for="maxNumber">最大人数</label>
+    <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
+    <p>{{ typeof(eventData.maxNumber) }}</p>
   </div>
 </template>
 
@@ -17,7 +21,7 @@
       return {
         eventData: {
           title: "ほげ",
-          text: "hogetext"
+          maxNumber: 0,
         }
       }
     }
