aa34e2b6124824b74cf4e5ba460261e198533190
sgash708 <sugash7081023@gmail.com>
2021-11-22 16:06:56 +0900

    L147. フィルタの複数適用

--- frontend/src/components/Section11.vue
+++ frontend/src/components/Section11.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
+    <!-- L145. グローバルfilter -->
     <h2>{{ title | upperCase }}</h2>
     <h2>{{ subTitle | upperCase }}</h2>
 
+    <!-- L146. ローカルfilter -->
     <h2>{{ title | lowerCase }}</h2>
     <h2>{{ subTitle | lowerCase }}</h2>
+
+    <!-- L147. 複数フィルタの適用 -->
+    <!-- -> パイプで繋ぐことで、結果をさらに次に実行する。shellのsedと同じ -->
+    <h2>{{ title | lowerCase | upperCase }}</h2>
+    <h2>{{ subTitle | lowerCase | upperCase }}</h2>
   </div>
 </template>
 
